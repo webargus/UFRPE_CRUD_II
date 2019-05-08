a39de672543adf5edb59ece1589801138439e811
bushman_svn <edsonkrop@yahoo.com>
2019-05-08 15:48:03 -0300

    designing program GUI

--- alunos.py
+++ alunos.py
@@ -10,18 +10,21 @@ class PainelAlunos:
 
         frame.grid_rowconfigure(0, weight=0)    # 0 => row won't expand vertically if there's space for that
         frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
 
         # cria frame p/ formulário de novo aluno
-        fNovoAluno = LabelFrame(frame, {"pady": 8, "padx": 8, "text": " Novo Cadastro "})
-        fNovoAluno.grid({"row": 0, "column": 0, "sticky": NSEW})
-        fNovoAluno.grid_columnconfigure(1, weight=1)
-        Label(fNovoAluno, {"text": "Nome:"}).grid({"row": 0, "column": 0})
-        self.nomeEdit = Entry(fNovoAluno).grid({"row": 0, "column": 1, "columnspan": 2, "sticky": "we"})
-        Label(fNovoAluno, {"text": "CPF:"}).grid({"row": 1, "column": 0, "sticky": W})
-        self.cpfEdit = Entry(fNovoAluno).grid({"row": 1, "column": 1, "sticky": W})
-        self.submitBtn = Button(fNovoAluno, {"text": "Ok", "width": 10}).grid({"row": 1, "column": 2, "pady": 8})
+        fAluno = LabelFrame(frame, {"pady": 8, "padx": 8, "text": " Cadastro "})
+        fAluno.grid({"row": 0, "column": 0, "sticky": NSEW, "pady": 8, "padx": 8})
+        fAluno.grid_columnconfigure(1, weight=1)
+        Label(fAluno, {"text": "Nome:"}).grid({"row": 0, "column": 0})
+        self.nomeEdit = Entry(fAluno).grid({"row": 0, "column": 1, "columnspan": 2, "sticky": "we"})
+        Label(fAluno, {"text": "CPF:"}).grid({"row": 1, "column": 0, "sticky": W})
+        self.cpfEdit = Entry(fAluno).grid({"row": 1, "column": 1, "sticky": W})
+        self.submitBtn = Button(fAluno, {"text": "Ok", "width": 10}).grid({"row": 1, "column": 2, "pady": 8})
 
         fTreeview = Frame(frame, {"relief": SUNKEN})
+        fTreeview.grid_columnconfigure(0, weight=1)
+        fTreeview.grid_rowconfigure(0, weight=1)
         fTreeview.grid({"row": 1, "column": 0, "sticky": NSEW})
         self.tree = tv.TreeViewTable(fTreeview, ("CPF", 'Aluno', 'Turma'))
         self.tree.appendItem(("278.700.814-34", "Edson Kropniczki", "TGA"))
--- treeviewtable.py
+++ treeviewtable.py
@@ -8,7 +8,7 @@ class TreeViewTable:
     def __init__(self, frame, headers):
         print("headers=", headers[1:])
         self.tree = ttk.Treeview(frame, columns=headers[1:], selectmode='extended')
-        self.tree.grid({"row": 0, "column": 0})
+        self.tree.grid({"row": 0, "column": 0, "sticky": NSEW})
         tree_scroll = ttk.Scrollbar(frame, orient=HORIZONTAL, command=self.tree.xview)
         tree_scroll.grid({"row": 1, "column": 0, "sticky": (W, E)})
         self.tree.configure(xscrollcommand=tree_scroll.set)
