41ed8014dde0ed7e205dd451da29897392ccdeb0
bushman_svn <edsonkrop@yahoo.com>
2019-05-24 10:01:43 -0300

    creating class LabelTable for 'ata'; trying to add scrollbars

--- drafts.py
+++ drafts.py
@@ -4,12 +4,16 @@ import tkinter as tk
 class ExampleApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+
         headers = {"ORD": (5, "center"),
                    "Nota": (8, "center"),
                    "Nome do(a) aluno(a)": (40, "w"),
                    "Assinatura": (40, "center")}
         t = LabelTable(self, headers)
-        t.pack(side="top", fill="x")
+        t.grid(row=0, column=0)
         t.cell_config(0, 2, {"anchor": "center"})
         t.insert([1, ' ', 'Ana Carolina Kropniczki de Azevedo', ' '])
         t.insert([2, ' ', 'Edson Kropniczki', ' '])
@@ -20,6 +24,7 @@ class LabelTable(tk.Frame):
     def __init__(self, parent, headers):
         # use gray background so it "peeks through" to form grid lines
         tk.Frame.__init__(self, parent, background="gray", padx=1, pady=1)
+
         self._cells = []
         self._widths = list(headers.values())
         self._headers = list(headers.keys())
